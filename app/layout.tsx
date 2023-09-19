2f7cd0a6974793f7d7f88ed9086f5650babb559f
Junaid <mzrjunaid@gmail.com>
2023-09-19 18:34:19 +0500

    social contact updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,33 @@
-import { Footer, Navbar } from '@/components'
-import { montserrat, anton } from './fonts'
-import './globals.css'
-import type { Metadata } from 'next'
+import { Footer, Navbar, SocialLinks } from "@/components";
+import { montserrat, anton } from "./fonts";
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Junaid Mazhar - Portfolio',
-  description: 'Hi, I am Junaid Mazhar, I develop Websites Front-End with (React, Nextjs, Bootstrap, Tailwind) and Mobile Applications with React Native',
-}
+  title: "Junaid Mazhar - Portfolio",
+  description:
+    "Hi, I am Junaid Mazhar, I develop Websites Front-End with (React, Nextjs, Bootstrap, Tailwind) and Mobile Applications with React Native",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${anton.variable} ${montserrat.variable} text-white relative`}>
-      <body className='bg-gradient-to-r from-blue-400 to-teal-400 via-purple-600 animate-gradient-x'>
+    <html
+      lang="en"
+      className={`${anton.variable} ${montserrat.variable} text-white relative`}
+    >
+      <body className="bg-gradient-to-r from-blue-400 to-teal-400 via-purple-600 animate-gradient-x">
         <Navbar />
-        <main className='font-montserrat'>{children}</main>
+        <main className="font-montserrat">{children}</main>
         <Footer />
+        <SocialLinks
+          size={28}
+          socialLinkStyle="flex flex-col gap-2 fixed bottom-16 right-3 bg-black bg-opacity-30 p-2 rounded-lg"
+        />
       </body>
     </html>
-  )
+  );
 }
--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,20 +1,61 @@
-import Link from 'next/link'
-import { FaLinkedin, FaSquareFacebook, FaSquareGithub, FaSquareInstagram } from 'react-icons/fa6'
+import Link from "next/link";
+import {
+  FaLinkedin,
+  FaSquareFacebook,
+  FaSquareGithub,
+  FaSquareInstagram,
+} from "react-icons/fa6";
 
 interface SocialProps {
-    socialLinkStyle?: string;
-    size: number;
+  socialLinkStyle?: string;
+  size: number;
 }
 
 const SocialLinks = ({ size, socialLinkStyle }: SocialProps) => {
-    return (
-        <div className={`flex flex-row gap-2 parent ${socialLinkStyle}`}>
-            <Link href={'https://www.linkedin.com/in/junaidmazhar/'} target='__blank'><FaLinkedin size={size} /></Link>
-            <Link href={'https://github.com/mzrjunaid'} target='__blank'><FaSquareGithub size={size} /></Link>
-            <Link href={'https://www.facebook.com/m.junaidmazhar'} target='__blank'><FaSquareFacebook size={size} /></Link>
-            <Link href={'https://www.instagram.com/junaid.muhamd/'} target='__blank'><FaSquareInstagram size={size} /></Link>
-        </div>
-    )
-}
+  return (
+    <div className={`parent ${socialLinkStyle}`}>
+      <Link
+        href={"https://www.linkedin.com/in/junaidmazhar/"}
+        target="__blank"
+        className="group/item relative"
+      >
+        <FaLinkedin size={size} />
+        <span className="invisible group-hover/item:visible absolute top-0 right-9 bg-black bg-opacity-80 py-1 px-3 rounded-lg">
+          @junaidmazhar
+        </span>
+      </Link>
+      <Link
+        href={"https://github.com/mzrjunaid"}
+        target="__blank"
+        className="group/item relative"
+      >
+        <FaSquareGithub size={size} />
+        <span className="invisible group-hover/item:visible absolute top-0 right-9 bg-black bg-opacity-80 py-1 px-3 rounded-lg">
+          @mzrjunaid
+        </span>
+      </Link>
+      <Link
+        href={"https://www.facebook.com/m.junaidmazhar"}
+        target="__blank"
+        className="group/item relative"
+      >
+        <FaSquareFacebook size={size} />
+        <span className="invisible group-hover/item:visible absolute top-0 right-9 bg-black bg-opacity-80 py-1 px-3 rounded-lg">
+          @m.junaidmazhar
+        </span>
+      </Link>
+      <Link
+        href={"https://www.instagram.com/junaid.muhamd/"}
+        target="__blank"
+        className="group/item relative"
+      >
+        <FaSquareInstagram size={size} />
+        <span className="invisible group-hover/item:visible absolute top-0 right-9 bg-black bg-opacity-80 py-1 px-3 rounded-lg">
+          @junaid.muhamd
+        </span>
+      </Link>
+    </div>
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
